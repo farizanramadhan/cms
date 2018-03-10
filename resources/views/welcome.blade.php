50b1d1e2da69fd27da7489442f5032ab428a025d
Farizan Ramadhan <farizanramadhan@MacBook-Air-Farizan.local>
2018-03-10 18:04:48 +0700

    edit welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CMS by Farizan
                 </div>
 
                 <div class="links">
